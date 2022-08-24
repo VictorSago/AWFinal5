8c28815d5019ee62d238aaf504e0783fa25c958e
Victor Sago <VictorSago01@gmail.com>
2022-08-24 20:15:53 +0200

    Dataset for lang_by_country as a dataframe

---
+++ langused_dataset.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+
+
+df2022 = pd.read_csv("rawdata/2022/survey_results_public.csv", dtype=str)
+
+# Create a dictionary the counter of all the 
+# used and wanted languages for each country
+langs_by_country = {}
+
+# For each available answer count the total answers for that country and
+# the used and wanted languages
+for row in df2022.itertuples():
+    # Check that the entry for Country is present
+    if type(row.Country) is str:
+        langs_by_country.setdefault(str(row.Country), {
+            'total': 0,
+            'lang_used': Counter(),
+            'lang_wanted': Counter()
+        })
+        langs_used = str(row.LanguageHaveWorkedWith).split(";")
+        langs_wanted = str(row.LanguageWantToWorkWith).split(";")
+        langs_by_country[row.Country]['total'] += 1
+        langs_by_country[row.Country]['lang_used'].update(langs_used)
+        langs_by_country[row.Country]['lang_wanted'].update(langs_wanted)
+
+# Sort the dict by country name
+#sorted_langs_by_country = {k: langs_by_country[k] for k in sorted(langs_by_country.keys())}
+
+# Reformat the previously created dict to a form suitable 
+# to be converted into a dataframe
+all_langs_used = []
+
+for country in langs_by_country:
+    total = langs_by_country[country]['total']
+    langs_used = {'name': country, 'total': total}
+    for lang in langs_by_country[country]['lang_used']:
+        langs_used[f'{lang}_used%'] = round(langs_by_country[country]['lang_used'][lang] * 100 / total, 2)
+    for lang in langs_by_country[country]['lang_wanted']:
+        langs_used[f'{lang}_wanted%'] = round(langs_by_country[country]['lang_wanted'][lang] * 100 / total, 2)
+    all_langs_used.append(langs_used)
+
+# Convert the resulting dict into a dataframe
+df_new = pd.DataFrame(all_langs_used)
+
+# Replace all N/A with 0s
+df_new.fillna(0, inplace=True)
+
+print(df_new)
+
+# Save the dataframe to a csv
+df_new.to_csv('cleaned/langs_used_by_country.csv', encoding='utf-8')
